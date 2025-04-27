20b82f653639a102f14fec30a49aa71d31f2d89a
Daniel Obasi <danielobasi@gmail.com>
2025-04-28 00:58:59 +0100

    Added Google maps to search page

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:moniepoint_realestate/constants.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,8 +13,80 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final Completer<GoogleMapController> _controller =
+  Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(defaultPadding),
+              child: Row(
+                children: [
+                  Container(
+                    width: 270,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: brandWhite,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Saint Petersburg",
+                        border: InputBorder.none,
+                        icon: SvgPicture.asset("assets/images/icons/magnifying-glass.svg", width: 24, height: 24,),
+                      ),
+
+                    )
+                  ),
+                  const SizedBox(width: defaultPadding),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: brandWhite,
+                      borderRadius: BorderRadius.circular(20),
+                  ),
+                    child: Center(
+                      child: SvgPicture.asset("assets/images/icons/sliders-horizontal.svg", width: 24, height: 24,),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+    );
   }
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
 }
